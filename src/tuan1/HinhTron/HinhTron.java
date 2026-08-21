87677341c9902c1dabb4275a83b8ffb05d6bf2dd
H82M19\Student <Student>
2026-08-21 20:11:59 +0700

    Cap nhat code hinh tron

--- src/tuan1/HinhTron/HinhTron.java
+++ src/tuan1/HinhTron/HinhTron.java
@@ -37,8 +37,9 @@ public class HinhTron {
 	public static void main(String[] args) {
 		HinhTron h1 = new HinhTron(new ToaDo("O", 4, 7), 10);
 		System.out.println("Thong tin hinh tron");
-		System.out.println("Hinh tron: Tam(" + h1.getTam().getTen() + ")");
+		System.out.println("Tam: " + h1.getTam().getTen() + "(" + h1.getTam().getX() + "," + h1.getTam().getY() + ")");
 		System.out.println("Hinh tron co ban kinh: " + h1.getBanKinh());
+
 	}
 
 }
