27667005af00869abe15ddcd90b2262350c5f5c7
H82M19\Student <Student>
2026-08-21 19:43:46 +0700

    Cap nhat ham tinhDienTich

--- src/tuan1/HinhChuNhat/HinhChuNhat.java
+++ src/tuan1/HinhChuNhat/HinhChuNhat.java
@@ -38,9 +38,14 @@ public class HinhChuNhat {
 		}
 	}
 
+	public double tinhDienTich() {
+		return chieuDai * chieuRong;
+	}
+
 	public static void main(String[] args) throws Exception {
 		HinhChuNhat h1 = new HinhChuNhat(5, 5);
 		System.out.println("Chieu rong h1: " + h1.getChieuRong());
 		System.out.println("Chieu dai h1: " + h1.getChieuDai());
+		System.out.println("Dien tich: " + h1.tinhDienTich());
 	}
 }
